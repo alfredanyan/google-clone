db925d2907eccce7594abd1954d81972afa08019
aanyan <a.anyan@zoovu.com>
2022-02-07 23:32:05 +0100

    app css

--- src/App.js
+++ src/App.js
@@ -4,9 +4,19 @@ import Header from "./components/header";
 import Sidebar from "./components/sidebar";
 import FilesView from "./components/filesView/FilesView";
 import SideIcons from "./components/sideIcons";
+import Glogo from "../../media/google2.png";
+import { auth, provider } from "./firebase";
+
 
 function App() {
   const [user, setUser] = useState();
+  const handleLogin = () => {
+      if(!user) {
+        auth.signInWithPopup(provider).then((result) => {
+          setUser(result.user);
+        })
+      }
+  }
   //authentication
 
   return (
@@ -21,7 +31,10 @@ function App() {
           </div>
         </>
       ) : (
-        <div className="app__main"></div>
+        <div className="app__login">
+          <img src={Glogo} alt="Google Drive" />
+          <button onClick = {handleLogin}>Log in to Google Drive</button>
+        </div>
       )}
       {/* <!-- auth true:
       - sidebar
